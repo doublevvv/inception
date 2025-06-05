c409c3bb926a8d7eed10d9f39d913a9d361d7aac
victoire <victoire.lggn@outlook.fr>
2025-06-05 12:40:15 +0200

    celui en cours

---
+++ inception_debian/mariadb/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [! -f "/vat/lib/mysql/$MYSQL_DATABASE"]; then
+    mariadb start
+    mariad -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+    mariad -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+    mariad -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+    mariad -e "FLUSH PRIVILEGES;"
+    mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+    exec mysqld_safe
+
+    fi
+    exec mariadbd --datadir=/var/lib/mysql
+
